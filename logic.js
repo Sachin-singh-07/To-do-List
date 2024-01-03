1c47d02ef96a1668a3edf63dde028cb028bcb7fe
sachin singh <sanisingh1437@gmail.com>
2024-01-03 22:53:41 +0530

    Initial commit

---
+++ logic.js
@@ -0,0 +1,59 @@
+let input = document.getElementById('input-text');
+let list = document.getElementById('list');
+
+let into = document.getElementById('list');
+let button = document.getElementById('btn');
+
+
+input.addEventListener('keyup', function (event) {
+  
+  if (event.keyCode === 13) {
+   
+    button.click();
+  }
+});
+
+
+
+function logic() {
+  if (input.value === '') {
+    alert("Please add some task");
+  } else {
+    console.log('list appended');
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    list.appendChild(li);
+
+    let span = document.createElement('span');
+    span.innerText = '\u00d7';
+    li.appendChild(span);
+
+
+  }
+  input.value = '';
+  savedata();
+}
+
+list.addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle("checked");
+    savedata();
+
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove();
+    savedata();
+  }
+}, false);
+
+
+function savedata() {
+  localStorage.setItem('data', list.innerHTML);
+}
+
+function showtask() {
+  list.innerHTML = localStorage.getItem('data');
+
+}
+
+
+showtask();
